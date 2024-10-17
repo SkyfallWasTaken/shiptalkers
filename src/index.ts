ea58adfbf24e5796b391093aec39785b0005c90f
SkyfallWasTaken <mahadkalam1@proton.me>
2024-10-17 19:29:12 +0100

    Schema updates

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { z } from "zod";
-import cookie from "cookie";
+import { createCanvas, loadImage } from "canvas";
 
 export const Env = z.object({
   WORKSPACE: z.string(),
@@ -14,30 +14,35 @@ env.D_TOKEN = decodeURIComponent(env.D_TOKEN);
 export const MemberActivity = z.object({
   username: z.string(),
   display_name: z.string(),
-  date_last_active: z.number().positive(),
-  messages_posted: z.number().positive(),
-  reactions_added: z.number().positive(),
-  days_active: z.number().positive(),
-  days_active_desktop: z.number().positive(),
-  days_active_android: z.number().positive(),
-  days_active_ios: z.number().positive(),
-  slack_huddles_count: z.number(),
+  date_last_active: z.number().nonnegative(),
+  messages_posted: z.number().nonnegative(),
+  reactions_added: z.number().nonnegative(),
+  days_active: z.number().nonnegative(),
+  days_active_desktop: z.number().nonnegative(),
+  days_active_android: z.number().nonnegative(),
+  days_active_ios: z.number().nonnegative(),
+  slack_huddles_count: z.number().nonnegative(),
 });
 export const AnalyticsResult = z.object({
   ok: z.boolean(),
-  num_found: z.number().min(1),
+  num_found: z.number().min(1).max(1),
   member_activity: z.array(MemberActivity).min(1),
 });
 
+const username = "mahadkalam1234"; // FIXME: change
 const formData = new FormData();
 formData.append("token", env.C_TOKEN);
 formData.append("date_range", "30d");
 formData.append("count", "1");
 formData.append("sort_column", "username");
 formData.append("sort_direction", "asc");
-formData.append("query", "mahadkalam1234"); // FIXME: change
-formData.append("_x_reason", "loadMembersDataForTimeRange");
+formData.append("query", username);
+formData.append("count", "1");
 
+const dbg = (...args: any[]) => {
+  console.debug(...args);
+  return args.length === 1 ? args[0] : args;
+};
 const response = await fetch(
   `https://${env.WORKSPACE}.slack.com/api/admin.analytics.getMemberAnalytics`,
   {
@@ -45,18 +50,15 @@ const response = await fetch(
     body: formData,
     headers: {
       Authority: `${env.WORKSPACE}.slack.com`,
-      Cookie: cookie.serialize("d", env.D_TOKEN),
+      Cookie: `d=${encodeURIComponent(env.D_TOKEN)}`, // We don't really need anything fancy here.
     },
   }
 );
-const data = AnalyticsResult.parse(await response.json());
+const data = AnalyticsResult.parse(dbg(await response.json()));
 if (!data.ok) {
   throw new Error("Failed to fetch analytics data");
 }
-if (!data.num_found) {
-  throw new Error("No users found");
-}
 const member = data.member_activity[0];
-console.log(
-  `${member.display_name} sent ${member.messages_posted} messages in the last 30 days`
-);
+if (member.username !== username) {
+  throw new Error(`${member.username} != ${username}`);
+}
